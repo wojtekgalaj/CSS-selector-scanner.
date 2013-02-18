cf058c4176d90bee2029e7ae8753a1b3bc0e01e2
wojtek <wojtek.galaj@gmail.com>
2013-02-18 23:24:01 +0100

    Can't quite get it right :/

--- app.js
+++ app.js
@@ -2,37 +2,45 @@ var fs = require('fs');
 var _ = require('underscore');
 var $ = require('jquery');
 
+var logNotFound = function (row) {
+  var logText = row.join().replace(/,/g, ' ');
+  console.log('Logged missed selector: ', logText);
+}
+
 var matchInMarkup = function (row, markup) {
   var $markup = $(markup),
-    rowLength = row.length - 1,
-    $matched;
+    selectorPosition = row.length - 1,
+    $matched,
+    selector = '',
+    firstRun;
 
-  while (rowLength >= 0) {
-    // 1. Find selector in the markup
-    console.log('Looking for ', row[rowLength]);
-    $matched = $markup.find(row[rowLength]);
+  console.log('New Row!')
+  while (selectorPosition >= 0) {
+    console.log('Looking for ', row[selectorPosition] + selector);
+    $matched = $markup.find(row[selectorPosition] + selector);
     console.log('matched: ', $matched.length)
     if (!$matched.length) {
       console.log('Havent found it, returning');
+      logNotFound(row);
       return;
     }
-    // 2. See if parent selector match aswell
-    // 3. Do nothing if selectors match
-    // 4. Log to a report file if selectors do not match
-    console.log('Decresing rowLength');
-    rowLength --;
+    firstRow = selector === '' ? ' ' : '';
+    selector = firstRow + row[selectorPosition] + ' ' + selector;
+    console.log('New selector: ', selector);
+    console.log('Decresing selectorPosition');
+    selectorPosition --;
   }
-}
+};
 
 var findCssSelectors = function (styles, markup) {
-  var selectorRows = styles.match(/.+\{/gim);
+  var selectorRows = styles.replace(/\{/g, ' {').match(/.+\{/gim);
 
   _.each(selectorRows, function (row, index) {
     row = row.replace(/ +/g, ' ' ).split(' ');
     row = _.without(row, '{');
     matchInMarkup(row, markup);
   })
-}
+};
 
 var markup = fs.readFileSync('testProject/index.html', 'utf-8');
 var styles = fs.readFileSync('testProject/css/styles.css', 'utf-8');
