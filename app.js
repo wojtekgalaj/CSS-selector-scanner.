b3c52652ccdce9b85e258f2a62b6f26644528e5d
Wojtek <wojtek@wojtek.(none)>
2013-04-09 18:01:48 +0200

    Moving right along

--- app.js
+++ app.js
@@ -2,39 +2,47 @@ var fs = require('fs');
 var _ = require('underscore');
 var $ = require('jquery');
 
-var matchInMarkup = function (row, markup) {
-  var $markup = $(markup),
-    rowLength = row.length - 1,
-    $matched;
-
-  while (rowLength >= 0) {
-    // 1. Find selector in the markup
-    console.log('Looking for ', row[rowLength]);
-    $matched = $markup.find(row[rowLength]);
-    console.log('matched: ', $matched.length)
-    if (!$matched.length) {
-      console.log('Havent found it, returning');
-      return;
-    }
-    // 2. See if parent selector match aswell
-    // 3. Do nothing if selectors match
-    // 4. Log to a report file if selectors do not match
-    console.log('Decresing rowLength');
-    rowLength --;
-  }
+var logNotFound = function (log) {
+  console.log('Logged missed selector: ', log);
 }
+
+var matchInMarkup = function (row, $markup) {
+  var selectorPosition = row.length - 1,
+    $matched,
+    selector = '',
+    firstRun;
+
+  if (!$markup.find(row).length) {
+    logNotFound(row);
+  }
+};
 
 var findCssSelectors = function (styles, markup) {
-  var selectorRows = styles.match(/.+\{/gim);
-
+  var selectorRows = styles.replace(/\{/g, ' {').match(/.+\{/gim),
+    $markup = $(markup);
+  
+  console.log('selectorRows: ', selectorRows);
+  
   _.each(selectorRows, function (row, index) {
     row = row.replace(/ +/g, ' ' ).split(' ');
-    row = _.without(row, '{');
-    matchInMarkup(row, markup);
+    row = _.without(row, '{').join().replace(/,/g, ' ');
+    matchInMarkup(row, $markup);
   })
-}
+};
 
 var markup = fs.readFileSync('testProject/index.html', 'utf-8');
 var styles = fs.readFileSync('testProject/css/styles.css', 'utf-8');
 
-findCssSelectors(styles, markup);
+// findCssSelectors(styles, markup);
+
+
+
+/*
+TODO:
+
+Grab all the xsl files and consrtruct a pseudoDOMtree to check css rules against.
+Grab all the css files and contrast them against the pseudoDOMtree.
+
+*/
+
+//var xslFolder = fs.readFileSync('/')
